c6fa77aa8083821d208e582fd1e15e441e50a32f
Paketsu <pauliina.veijalainen@gmail.com>
2019-08-22 14:17:45 +0300

    Autoharjoitus
    Tehtävänanto:
    Lisää luokkaan Auto seuraavat yksityiset (private) ominaisuudet
    * tankinTilavuus
    * bensanMaara
    Lisää luokkaan seuraavat omat metodit
    * käynnisty-metodi, joka kertoo auton käynnistyneen ja näyttää käynnistyneen auton bensan määrän sekä auton tankin tilavuuden.
    * tulostaAutonTiedot, joka tulostaa kaikki auton ominaisuudet.
    * lisääBensaa($maara)-metodi, jolla voi lisätä autoon bensaa. Metodi näyttää lisäyksen tankissa olevan bensan määrän.
    * aja-metodi, jolla autoa voi ajaa. Jokainen ajo vähentää bensaa yhdellä litralla.
    * sammuta-metodi, joka kertoo auton sammuneen.

---
+++ PHP/Olio-ohjelmointi 2/auto.php
@@ -0,0 +1,40 @@
+<?php
+    class Auto {
+        private $tankinTilavuus;
+        private $bensanMaara;
+        public $merkki;
+
+        public function __construct($tilavuutta, $bensaa) {
+            $this->tankinTilavuus = $tilavuutta;
+            $this->bensanMaara = $bensaa;
+            $this->merkki = "Audi";
+            $this->vuosimalli = 2015;
+        }
+
+        public function tulostaTankki(){
+            echo $this->tankinTilavuus;
+        }
+
+        public function kaynnisty() {
+            echo "Auto on käynnissä! Tankin tilavuus on " . $this->tankinTilavuus . " litraa ja bensaa on jäljellä " . $this->bensanMaara . " litraa.<br>";
+        }
+
+        public function aja() {
+            $this->bensanMaara = $this->bensanMaara - 1;
+            return "Käynnistit auton, tankissa on nyt jäljellä " . $this->bensanMaara . " litraa bensaa.<br>";
+        }
+
+        public function lisaaBensaa($maara) {
+            $enemmanBensaa = $this->bensanMaara + $maara;
+            return  "Tankissa on nyt " . $enemmanBensaa . " litraa bensaa.<br>";
+        }
+
+        public function sammuta() {
+            return "Auto on nyt sammutettu.<br>";
+        }
+
+        public function tulostaTiedot() {
+            return "Auto on " . $this->merkki . " vuodelta " . $this->vuosimalli . ". Sen tankin tilavuus on " . $this->tankinTilavuus . " ja bensaa on jäljellä vielä " . $this->bensanMaara . ".<br>";
+        }
+}
+?>
